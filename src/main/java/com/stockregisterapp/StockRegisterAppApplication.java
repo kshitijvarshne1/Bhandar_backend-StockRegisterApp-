7504463b76208d32f9504499d69ba0e9d450eba8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-29 18:00:36 +0530

    Initial commit

---
+++ src/main/java/com/stockregisterapp/StockRegisterAppApplication.java
@@ -0,0 +1,13 @@
+package com.stockregisterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRegisterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockRegisterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stockregisterapp/StockRegisterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockregisterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRegisterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
